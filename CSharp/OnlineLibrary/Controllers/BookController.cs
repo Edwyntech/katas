7831fc77a85f765bceac13260c5de3adc2b60f00
CAYROL-EXT Jérôme <jerome.cayrol-ext@fnacdarty.com>
2023-01-26 17:33:24 +0100

    Delete copied coded

--- CSharp/OnlineLibrary/Controllers/BookController.cs
+++ CSharp/OnlineLibrary/Controllers/BookController.cs
@@ -18,30 +18,6 @@ public class BookController : ControllerBase
     [HttpGet]
     public IEnumerable<Book> GetAll()
     {
-        using var db = new OnlineLibraryContext(new DbContextOptions<OnlineLibraryContext>());
-
-// Create
-        Console.WriteLine("Inserting a new blog");
-        db.Add(new Book() { Title = "http://blogs.msdn.com/adonet" });
-        db.SaveChanges();
-
-// Read
-        Console.WriteLine("Querying for a blog");
-        var blog = db.Books
-            .OrderBy(b => b.Id)
-            .First();
-
-// Update
-        Console.WriteLine("Updating the blog and adding a post");
-        blog.Title = "https://devblogs.microsoft.com/dotnet";
-        db.SaveChanges();
-
-// Delete
-        Console.WriteLine("Delete the blog");
-        db.Remove(blog);
-        db.SaveChanges();
-
-        return null;
-        //return _bookService.GetAllBooks();
+        return _bookService.GetAllBooks();
     }
 }
